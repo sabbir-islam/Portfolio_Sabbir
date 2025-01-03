10da544a8ae95f2d1335d370999d725bc12a0df6
sabbir-islam <sabbir3921@gmail.com>
2025-01-04 00:49:47 +0600

    device responsibeness

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById("navbar-toggle").addEventListener("click", function () {
+  const navbar = document.getElementById("navbar-default");
+  navbar.classList.toggle("hidden");
+});
