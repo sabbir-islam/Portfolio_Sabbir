a31c89571fbcfc788cef9f8e842d82cf9f0531ad
sabbir-islam <sabbir3921@gmail.com>
2025-01-04 18:35:17 +0600

    link added

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ document.getElementById("navbar-toggle").addEventListener("click", function () {
   const navbar = document.getElementById("navbar-default");
   navbar.classList.toggle("hidden");
 });
+
+document.getElementById("github").addEventListener("click", function () {
+  window.open("https://github.com/sabbir-islam", "_blank");
+});
+
+document.getElementById("linkedin").addEventListener("click", function () {
+  window.open("https://www.linkedin.com/in/sabbir-islam-109b12256", "_blank");
+});
